4d2c49cb3279938a3e3f154712aa9c44886139b2
PieterDr <pieter.drijkoningen@vrt.be>
2018-03-01 19:57:47 +0100

    First solve attempt

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,18 +2,33 @@ package main;
 
 import main.exercise.*;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        List<String> input = new Reader("a_example.in").readLines();
+        String[] files = new String[]{
+                "a_example.in",
+                "b_should_be_easy.in",
+                "c_no_hurry.in",
+                "d_metropolis.in",
+                "e_high_bonus.in"
+        };
+        for (String file: files) {
+            solve(file);
+        }
+    }
+
+    private static void solve(String fileName) throws IOException, URISyntaxException {
+        List<String> input = new Reader(fileName).readLines();
         String header = input.get(0);
         Grid grid = InputConverter.getGrid(header);
         List<Car> cars = InputConverter.getCars(header);
         List<Ride> rides = InputConverter.getRides(input.subList(1, input.size()));
 
         List<String> output = Solver.solve(grid, cars, rides);
-        new Writer("output/a.out").write(output);
+        new Writer("output/" + fileName + ".out").write(output);
     }
 }
--- src/main/java/main/exercise/Solver.java
+++ src/main/java/main/exercise/Solver.java
@@ -1,13 +1,20 @@
 package main.exercise;
 
-import java.util.Collections;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class Solver {
 
     public static List<String> solve(Grid grid, List<Car> cars, List<Ride> rides) {
-        return Collections.emptyList();
+        int carNr = 0;
+        for (Ride ride: rides) {
+            cars.get(carNr).assign(ride);
+            carNr++;
+            if (carNr == cars.size()) {
+                carNr = 0;
+            }
+        }
+        return cars.stream().map(Car::toString).collect(toList());
     }
-
-    //Write algorithm here
 }
