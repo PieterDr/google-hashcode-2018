4a4f35410593b3735f627328c295e7ab6fdfa2ee
PieterDr <pieter.drijkoningen@vrt.be>
2018-03-01 19:02:29 +0100

    Fix shizzle

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,11 @@
 package main;
 
-import main.exercise.Example;
-
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         List<String> input = new Reader("a_example.in").readLines();
-        new Writer("output/a.out").write(new Example(input).solve());
+        new Writer("output/a.out").write(input);
     }
 }
