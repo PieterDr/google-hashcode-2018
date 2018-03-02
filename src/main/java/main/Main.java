11f88e78368cd20d018af0d1a0731436aca671cb
Rob Kenis <rob.kenis@vrt.be>
2018-03-02 15:41:48 +0100

    Fix all the bugs

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String[] files = new String[]{
-                "a_example.in",
+     /*   String[] files = new String[]{
                 "b_should_be_easy.in",
                 "c_no_hurry.in",
-                "d_metropolis.in",
                 "e_high_bonus.in"
         };
-        for (String file: files) {
+        for (String file : files) {
             solve(file);
         }
+        solveA();*/
+        solveD();
     }
 
     private static void solve(String fileName) throws IOException, URISyntaxException {
@@ -29,6 +29,29 @@ public class Main {
         List<Ride> rides = InputConverter.getRides(input.subList(1, input.size()));
 
         List<String> output = Solver.solve(grid, cars, rides);
+        new Writer("output/" + fileName + ".out").write(output);
+    }
+
+    private static void solveA() throws URISyntaxException, IOException {
+        String fileName = "a_example.in";
+        List<String> input = new Reader(fileName).readLines();
+        String header = input.get(0);
+        List<Car> cars = InputConverter.getCars(header);
+        List<Ride> rides = InputConverter.getRides(input.subList(1, input.size()));
+
+        List<String> output = Solver.firstTry(cars, rides);
+        new Writer("output/" + fileName + ".out").write(output);
+    }
+
+    private static void solveD() throws URISyntaxException, IOException {
+        String fileName = "d_metropolis.in";
+        List<String> input = new Reader(fileName).readLines();
+        String header = input.get(0);
+        Grid grid = InputConverter.getGrid(header);
+        List<Car> cars = InputConverter.getCars(header);
+        List<Ride> rides = InputConverter.getRides(input.subList(1, input.size()));
+
+        List<String> output = Solver.tryMetropolis(cars, rides, grid);
         new Writer("output/" + fileName + ".out").write(output);
     }
 }
--- src/main/java/main/exercise/Solver.java
+++ src/main/java/main/exercise/Solver.java
@@ -7,8 +7,13 @@ import static java.util.stream.Collectors.toList;
 public class Solver {
 
     public static Grid GRID;
+    public static int count = 0;
 
     public static List<String> solve(Grid grid, List<Car> cars, List<Ride> rides) {
+        int maxLoops = (int) Math.floor(rides.size() / cars.size());
+        for (int i = 0; i < maxLoops; i++) {
+            giveBestRides(cars, rides, grid);
+        }
         GRID = grid;
         int ridesPerCar = (int) Math.ceil((double) rides.size() / (double) cars.size());
         return cars.stream()
@@ -17,14 +22,30 @@ public class Solver {
                 .collect(toList());
     }
 
-    private static void assignRides(Car car, List<Ride> rides, int numberOfRides) {
+    public static List<String> tryMetropolis(List<Car>cars, List<Ride>rides, Grid grid){
+        GRID = grid;
+        int ridesPerCar = (int) Math.ceil((double) rides.size() / (double) cars.size());
+        return cars.stream()
+                .peek(car -> assignRides(car, rides, ridesPerCar))
+                .map(Car::toString)
+                .collect(toList());
+    }
+
+    public static void giveBestRides(List<Car> cars, List<Ride> rides, Grid grid) {
+        cars.forEach(car -> {
+            Ride closestRide = Util.findClosestAchievableRide(car, rides, grid);
+            Util.assignAndRemoveFromInput(car, rides, closestRide);
+        });
+    }
+
+    public static void assignRides(Car car, List<Ride> rides, int numberOfRides) {
         while (car.rides.size() < numberOfRides && !rides.isEmpty()) {
             Ride closestRide = Util.findClosestAchievableRide(car, rides, GRID);
             Util.assignAndRemoveFromInput(car, rides, closestRide);
         }
     }
 
-    private static List<String> firstTry(List<Car> cars, List<Ride> rides) {
+    public static List<String> firstTry(List<Car> cars, List<Ride> rides) {
         Util.assignSequentiallyToCars(cars, rides);
         return Util.mapToOutputLine(cars);
     }
